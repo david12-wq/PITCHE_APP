88d94169f1f4fec0a17fe734d3d3d87265605408
david12-wq <davidkamau245@gmail.com>
2020-12-08 12:06:18 +0300

    add

--- config.py
+++ config.py
@@ -1,33 +1,52 @@
 import os
-
 class Config:
-    """
+    '''
     General configuration parent class
-    """
+    '''
+    SECRET_KEY = 'kama'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-    #email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+     #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitch'
+    SENDER_EMAIL = 'davidkamau245@gmail.com'
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    """
-    Production configuration
-    """
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Accessb@localhost/pitch'
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
     DEBUG = True
-
-
+    ENV = 'development'
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig,
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 }
